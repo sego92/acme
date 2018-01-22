658892977b4fc476c994d0fec7069dd0580fd5db
sego <sego.maris@gmail.com>
2018-01-22 13:52:04 +0100

    posts list added

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog/index.html')
+    posts = ['First post', 'Second Post', 'Third Post']
+    return render(request, 'blog/index.html', {'posts': posts})

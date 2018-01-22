750d24c87442252b76cdfeae0684e7d35195b12e
sego <sego.maris@gmail.com>
2018-01-22 11:47:10 +0100

    modification de la page d'accueil

--- acme/settings.py
+++ acme/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'acme.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 

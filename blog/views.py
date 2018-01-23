72d3e18b35adab243e8ce6d1185e8fa889f67b4e
sego92 <sego.maris@gmail.com>
2018-01-23 20:27:32 +0100

    ajout des articles, modifs views et models

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
-from .mocks import Post
+#from .mocks import Post
+from .models import Post
 
 # Create your views here.
 def index(request):
-    posts = Post.all()
+    posts = Post.objects.all()
     return render(request, 'blog/index.html', {'posts': posts})
 
 def show(request, id):
-    post = Post.find(id)
+    post = Post.objects.get(pk=id)
     return render(request, 'blog/show.html', {'post': post})
     

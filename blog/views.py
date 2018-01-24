bbf4b4f12889be8285e93e8a56545eaed7ce32fa
sego92 <sego.maris@gmail.com>
2018-01-24 11:31:51 +0100

    resolution erreur 404 quand article nonexistant

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 #from .mocks import Post
 from .models import Post
@@ -9,6 +10,10 @@ def index(request):
     return render(request, 'blog/index.html', {'posts': posts})
 
 def show(request, id):
-    post = Post.objects.get(pk=id)
+    post = get_object_or_404(Post, pk=id)
+    """try:
+        post = Post.objects.get(pk=id)
+    except Post.DoesNotExist:
+        raise Http404('Sorry the post #{} was not found.'.format(id))"""
     return render(request, 'blog/show.html', {'post': post})
     
